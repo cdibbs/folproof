d09ba15e52beeaeae882444e464fd5d1708db2ca
Chris Dibbern <chrisdibbern@gmail.com>
2014-04-10 21:57:30 -0500

    All rules implemented - I think I'm done! But probably not... :-)

--- verifier.js
+++ verifier.js
@@ -63,32 +63,37 @@ var foljsVerifier = (function() {
 
 	function preprocess(ast) {
 		var proof = { steps : [] };
-		preprocessBox(proof, ast, 0);
+		preprocessBox(proof, ast, 0, []);
 		return proof;
 	}
 
-	function preprocessBox(proof, ast, step) {
+	function preprocessBox(proof, ast, step, scope) {
 		for(var i=0; i<ast.length; i++) {
 			if (ast[i][0] === 'rule') {
-				proof.steps[step] = new Rule(ast[i][1], ast[i][2], ast[i][3]);
+				proof.steps[step] = new Rule(ast[i][1], ast[i][2], scope, ast[i][3]);
 				step = step + 1;
 			} else if (ast[i][0] === 'folbox') {
-				step = preprocessBox(proof, ast[i][1], step);
+				var newScope = scope.slice(0)
+				newScope.push([ast[i][2][1], ast[i][2][2]]);
+				step = preprocessBox(proof, ast[i][1], step, newScope);
 			} else if (ast[i][0] === 'box') {
-				step = preprocessBox(proof, ast[i][1], step);
+				step = preprocessBox(proof, ast[i][1], step, scope);
 			}
 		}
 		return step;
 	}
 
-	var Rule = function(sentenceAST, justificationAST, loc) {
+	var Rule = function(sentenceAST, justificationAST, scope, loc) {
 		this.getSentence = function getSentence() {
 			return sentenceAST;
 		};
+		this.getScope = function getScope() {
+			return scope;
+		}
 		this.getJustification = function getJustification() {
 			return justificationAST;
 		};
-		this.getMeta = function() {  return loc; }
+		this.getMeta = function() { return loc; }
 	};
 
 	var rules = {
@@ -172,7 +177,30 @@ var foljsVerifier = (function() {
 		},
 		"and" : {
 			"introduction" : function(proof, step, part, steps) {
-				return false;
+				debug("and-i", step, part, steps);
+				if (part != null)
+					return "And-Intro: Step part (e.g., the 2 in 'and e2') not applicable, in this context.";
+
+				if (steps.length != 2)
+					return "And-Intro: Requires two, prior-step references.";
+				var andSteps = [parseInt(steps[0]) - 1, parseInt(steps[1]) - 1];
+
+				if (andSteps[0] >= step || andSteps[1] >= step)
+					return "And-Intro: Both steps must precede the current step.";
+
+				var s = proof.steps[step].getSentence();
+				if (s[0] !== 'and')
+					return "And-Intro: Current step is not an 'and'-expression." + proof.steps[step].getSentence();
+
+				if (semanticEq(s[1], proof.steps[andSteps[0]].getSentence())) {
+					if (semanticEq(s[2], proof.steps[andSteps[1]].getSentence())) {
+						return true;
+					} else {
+						return "And-Intro: Right side doesn't match referenced step.";
+					}
+				}
+				
+				return "And-Intro: Left side doesn't match referenced step.";
 			},
 			"elimination" : function(proof, step, part, steps) {
 				debug("and-e", step, part, steps);
@@ -196,10 +224,64 @@ var foljsVerifier = (function() {
 		},
 		"or" : {
 			"introduction" : function(proof, step, part, steps) {
-				return false;
+				debug("or-i", step, part, steps);
+				if (part != 1 && part != 2)
+					return "Or-Intro: Must reference step side 1 or 2 (e.g., the 2 in 'or i2 n').";
+
+				if (steps.length != 1)
+					return "Or-Intro: Requires one, prior-step reference.";
+				var orStep = parseInt(steps[0]) - 1;
+
+				if (orStep >= step)
+					return "Or-Intro: Referenced step must precede the current step.";
+
+				var s = proof.steps[step].getSentence();
+				if (s[0] !== 'or')
+					return "Or-Intro: Current step is not an 'or'-expression.";
+
+				if (semanticEq(s[part], proof.steps[orStep].getSentence()))
+					return true;
+
+				return "Or-Intro: Side " + part + " doesn't match referenced step.";
 			},
 			"elimination" : function(proof, step, part, steps) {
-				return false;
+				debug("or-e", step, part, steps);
+				if (part != null)
+					return "Or-Elim: Step part (e.g., 2 in 'and e2') not applicable, in this context.";
+
+				if (steps.length != 3 || steps[1].split("-").length != 2 || steps[2].split("-").length != 2)
+					return "Or-Elim: Requires an 'or' step, and two assumption ranges: a, b-c, d-e.";
+
+				var orStep = parseInt(steps[0]) - 1;
+				var assm1StepR = steps[1].split("-");
+				assm1StepR = [parseInt(assm1StepR[0]) - 1, parseInt(assm1StepR[1]) - 1];
+				var assm2StepR = steps[2].split("-");
+				assm2StepR = [parseInt(assm2StepR[0]) - 1, parseInt(assm2StepR[1]) - 1];
+
+				if (orStep >= step || assm1StepR[0] >= step || assm1StepR[1] >= step
+					|| assm2StepR[0] >= step || assm2StepR[1] >= step)
+					return "Or-Elim: Referenced step must precede the current step.";
+				
+				var currStepExpr = proof.steps[step].getSentence();
+				var orStepExpr = proof.steps[orStep].getSentence();
+				var a1p1Expr = proof.steps[assm1StepR[0]].getSentence();
+				var a1p2Expr = proof.steps[assm1StepR[1]].getSentence();
+				var a2p1Expr = proof.steps[assm2StepR[0]].getSentence();
+				var a2p2Expr = proof.steps[assm2StepR[1]].getSentence();
+				
+				// and through the gauntlet...
+				if (orStepExpr[0] !== 'or')
+					return "Or-Elim: First referenced step is not an 'or'-expression.";
+				if (!semanticEq(orStepExpr[1], a1p1Expr))
+					return "Or-Elim: First range intro doesn't match left side of 'or'.";
+				if (!semanticEq(orStepExpr[2], a2p1Expr))
+					return "Or-Elim: Second range range intro doesn't match right side of 'or'.";
+				if (!semanticEq(a1p2Expr, a2p2Expr))
+					return "Or-Elim: Step range conclusions don't match.";
+				if (!semanticEq(a1p2Expr, currStepExpr))
+					return "Or-Elim: Current step doesn't match step range conclusions.";
+
+				return true;
 			}
 		},
 		"not" : {
@@ -259,20 +341,142 @@ var foljsVerifier = (function() {
 				return "Not-Elim: Subexpression in not-expr does not match other expr.";
 			}
 		},
-		"A.x" : {
+		"a.x" : {
 			"introduction" : function(proof, step, part, steps) {
-				return false;
+				debug("all-x-i", step, part, steps);
+				if (part != null)
+					return "All-x-Intro: Step part (e.g., the 2 in 'and e2') not applicable, in this context.";
+
+				if (steps.length != 1 || steps[0].split('-').length != 2)
+					return "All-x-Intro: Must reference one range, a-b.";
+				
+				var stepRange = steps[0].split("-");
+				stepRange = [parseInt(stepRange[0]) - 1, parseInt(stepRange[1]) - 1];
+				if (stepRange[0] >= step || stepRange[1] >= step || stepRange[0] > stepRange[1])
+					return "All-x-Intro: Referenced proof step range, x-y, must precede current step, and x < y.";
+			
+				var currStep = proof.steps[step];
+				var currExpr = currStep.getSentence();
+				var startStep = proof.steps[stepRange[0]];
+				var startExpr = startStep.getSentence();
+				var scope = startStep.getScope(); // ex: [['x0','x'], ['y0', 'y'], ...], LIFO
+				var endExpr = proof.steps[stepRange[1]].getSentence();
+				if (currExpr[0] !== 'forall')
+					return "All-x-Intro: Current step is not a 'for-all' expression.";
+				if (scope.length == 0)
+					return "All-x-Intro: Not valid without a scoping assumption (e.g., an x0 box).";
+				
+				// check if any substitutions from our scope match refExpr
+				var scopeVars = scope[scope.length-1];
+				var endExprSub = substitute(endExpr, scopeVars[0], scopeVars[1]);
+				if (semanticEq(endExprSub, currExpr[2]))
+					return true;
+				return "All-x-Intro: Last step in range doesn't match current step after " + scopeVars[0] + "/" + scopeVars[1] + ".";
 			},
 			"elimination" : function(proof, step, part, steps) {
-				return false;
+				debug("all-x-e", step, part, steps);
+				if (part != null)
+					return "All-x-Elim: Step part (e.g., the 2 in 'and e2') not applicable, in this context.";
+
+				if (steps == null || steps.length != 1)
+					return "All-x-Elim: Must reference one prior proof step.";
+				
+				var refStep = parseInt(steps[0]) - 1;
+				if (refStep >= step)
+					return "All-x-Elim: Referenced step must occur before current step.";
+			
+				var currStep = proof.steps[step];
+				var currExpr = currStep.getSentence();
+				var scope = currStep.getScope(); // ex: [['x0','x'], ['y0', 'y'], ...], LIFO
+				var refExpr = proof.steps[refStep].getSentence();
+				if (refExpr[0] !== 'forall')
+					return "All-x-Elim: Referenced step is not a for-all expression.";
+				if (scope.length == 0)
+					return "All-x-Elim: Not valid outside an assumption scope (e.g., an x0 box).";
+				
+				// check if any substitutions from our scope match refExpr
+				var checked = [];
+				for (var i=scope.length-1; i>=0; i--) {
+					checked.push(scope[i][0]);
+					var refExprSub = substitute(refExpr[2], scope[i][1], scope[i][0]);
+					if (semanticEq(refExprSub, currExpr))
+						return true;
+				}
+
+				return "All-x-Elim: Referenced step did not match current step under: " + checked.join(", ") + ".";
 			}
 		},
-		"E.x" : {
+		"e.x" : {
 			"introduction" : function(proof, step, part, steps) {
-				return false;
+				debug("exists-x-i", step, part, steps);
+				if (part != null)
+					return "Exists-x-Intro: Step part (e.g., the 2 in 'and e2') not applicable, in this context.";
+
+				if (steps == null || steps.length != 1)
+					return "Exists-x-Intro: Must reference one prior proof step.";
+				
+				var refStep = parseInt(steps[0]) - 1;
+				if (refStep >= step)
+					return "Exists-x-Intro: Referenced step must occur before current step.";
+			
+				var currStep = proof.steps[step];
+				var currExpr = currStep.getSentence();
+				var scope = currStep.getScope(); // ex: [['x0','x'], ['y0', 'y'], ...], LIFO
+				var refExpr = proof.steps[refStep].getSentence();
+				if (currExpr[0] !== 'exists')
+					return "Exists-x-Intro: Current step is not an 'exists' expression.";
+				if (scope.length == 0)
+					return "Exists-x-Intro: Not valid outside an assumption scope (e.g., an x0 box).";
+				
+				// check if any substitutions from our scope match refExpr
+				var checked = [];
+				for (var i=scope.length-1; i>=0; i--) {
+					checked.push(scope[i][0]);
+					var refExprSub = substitute(refExpr, scope[i][0], scope[i][1]);
+					if (semanticEq(refExprSub, currExpr[2]))
+						return true;
+				}
+
+				return "Exists-x-Elim: Referenced step did not match current step under: " + checked.join(", ") + ".";
 			},
 			"elimination" : function(proof, step, part, steps) {
-				return false;
+				debug("exists-x-e", step, part, steps);
+				if (part != null)
+					return "Exists-x-Elim: Step part (e.g., the 2 in 'and e2') not applicable, in this context.";
+
+				if (steps.length != 2 || steps[1].split('-').length != 2)
+					return "Exists-x-Elim: Must reference one prior proof step, and one range: a, b-c.";
+				
+				var refStep = parseInt(steps[0]) - 1;
+				if (refStep >= step)
+					return "Exists-x-Elim: Referenced step must occur before current step.";
+
+				var stepRange = steps[1].split("-");
+				stepRange = [parseInt(stepRange[0]) - 1, parseInt(stepRange[1]) - 1];
+				if (stepRange[0] >= step || stepRange[1] >= step || stepRange[0] > stepRange[1])
+					return "Exists-x-Elim: Referenced proof step range, x-y, must precede current step, and x < y.";
+			
+				var currStep = proof.steps[step];
+				var currExpr = currStep.getSentence();
+				var refExpr = proof.steps[refStep].getSentence();
+				var startStep = proof.steps[stepRange[0]];
+				var startExpr = startStep.getSentence();
+				var scope = startStep.getScope(); // ex: [['x0','x'], ['y0', 'y'], ...], LIFO
+				var endExpr = proof.steps[stepRange[1]].getSentence();
+				if (refExpr[0] !== 'exists')
+					return "Exists-x-Elim: Referenced step is not an 'exists' expression.";
+				if (scope.length == 0)
+					return "Exists-x-Elim: Not valid outside an assumption scope (e.g., an x0 box).";
+				
+				// check if any substitutions from our scope match refExpr
+				var scopeVars = scope[scope.length-1];
+				var refExprSub = substitute(refExpr[2], scopeVars[1], scopeVars[0]);
+				if (semanticEq(refExprSub, startExpr)) {
+					if (semanticEq(endExpr, currExpr))
+						return true;
+					return "Exists-x-Elim: assumption ending step does not match current step.";
+				}
+				return "Exists-x-Elim: assumption beginning step doesn't match ref step for " + scopeVars[0] + ".";
 			}
 		},	
 		"=" : {
@@ -319,6 +523,47 @@ var foljsVerifier = (function() {
 			}
 		},
 	};
+
+	function substitute(startExpr, a, b, bound) {
+		debug("substitute", startExpr, a, b);
+		bound = bound ? bound : [];
+		var binOps = ["->", "and", "or", "<->", "="];
+		var unOps = ["not", "forall", "exists"];
+
+		// remove parens, which are basically stylistic no-ops
+		while (startExpr[0] === 'paren') startExpr = startExpr[1];
+
+		if (arrayContains(binOps, startExpr[0])) {
+			var leftSide = substitute(startExpr[1], a, b);
+			var rightSide = substitute(startExpr[2], a, b);
+			return [startExpr[0], leftSide, rightSide];
+		} else if (arrayContains(unOps, startExpr[0])) {
+			if (startExpr[0] === "forall" || startExpr[0] === "exists") {
+				bound = bound.slice(0);
+				bound.push(startExpr[1]);
+				return [startExpr[0], startExpr[1],
+					substitute(startExpr[2], a, b, bound)];
+			}
+			
+			return [startExpr[0], substitute(startExpr[1], a, b, bound)];
+		} else if (startExpr[0] === 'id') {
+			if (startExpr.length === 2) { // our loverly base case
+				if (! arrayContains(bound, startExpr[1])) {
+					if (startExpr[1] === a)
+						return [startExpr[0], b];
+				}
+				return startExpr;
+			}
+			if (startExpr.length === 3) {
+				var newTerms = [];
+				for (var i=0; i<startExpr[2].length; i++) {
+					newTerms.push(substitute(startExpr[2][i], a, b, bound));
+				}
+				return [startExpr[0], startExpr[1], newTerms];
+			}
+			throw Error("Unexpected AST format.");
+		}
+	}
 
 	function elimTransform(startExpr, origSubExpr, newSubExpr) {
 		debug("elimTransform", startExpr, origSubExpr, newSubExpr);
@@ -387,6 +632,13 @@ var foljsVerifier = (function() {
 				return true;
 			}
 			debug("sEq path 2.1");
+			return false;
+		} else if (a[0] === 'exists' || a[0] === 'forall' && a[0] === b[0]) {
+			if (semanticEq(a[2], b[2])) {
+				debug("sEq path 2.5");
+				return true;
+			}
+			debug("sEq path 2.6");
 			return false;
 		} else if (a[0] === "id") {
 			if (a[1] !== b[1]) return false;
